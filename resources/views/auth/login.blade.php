a217aecde613014ffb357a72d6e9fd87e4519fdc
Mochamad Ludfi Rahman <ludfyrahman@Mochamads-MacBook-Pro.local>
2024-06-23 01:27:15 +0700

    update bg login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,11 @@
 @section('content')
 <div class="row no-gutter">
     <!-- The image half -->
-    <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-        <div class="row wd-100p mx-auto text-center">
-            <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-                <img src="" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-            </div>
-        </div>
+    <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent" style="
+    background: url('{{asset('assets/img/bg.jpg')}}') no-repeat center center;
+    background-size: cover;
+    background-repeat: none;">
+
     </div>
     <!-- The content half -->
     <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
